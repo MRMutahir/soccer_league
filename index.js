2229b279316de0fe15524b741cd8fb4cf3e7e613
MRMutahir <mutahirkareem820@gmail.com>
2024-03-16 09:42:02 -0700

    useNewUrlParser  remove

--- index.js
+++ index.js
@@ -13,10 +13,7 @@ let PORT = process.env.PORT || 3000;
 dotenv.config();
 const connect = () => {
   mongoose
-    .connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    })
+    .connect(process.env.MONGO_URI)
     .then(() => {
       console.log("Connected to MongoDB");
     })
