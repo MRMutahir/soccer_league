3e0bbabb1fb2d89c9497964fe4cdb78485957835
MuhammadEshareeb <eshareebm@gmail.com>
2024-03-16 06:30:47 -0700

    get_Upcoming_Matches

--- Controller/Upcoming_Matches.js
+++ Controller/Upcoming_Matches.js
@@ -1,19 +1,32 @@
 import { Team } from "../Modal/Team.js";
 
+const checkMatchExistence = async (teamId, matchDate) => {
+  const team = await Team.findById(teamId);
+  const matches = team.upcomingMatches.filter(
+    (match) => match.date.toDateString() === matchDate.toDateString()
+  );
+  return matches.length > 0;
+};
+
 const get_Upcoming_Matches = async (req, res, next) => {
   try {
     const teamId = req.params.teamId;
     const team = await Team.findById(teamId);
-    const upComingMatchesPreData = team.upcomingMatches;
-    const { date, time } = upComingMatchesPreData;
-    // console.log(
-    //   upComingMatchesPreData,
-    //   ">>>>>>>>>>>>>>>>>upComingMatchesPreData"
-    // );
     const body = req.body;
+    const matchDate = new Date(body.date);
+
+    const matchExists = await checkMatchExistence(teamId, matchDate);
+
+    if (matchExists) {
+      return res.status(400).json({
+        error:
+          "A match for this date already exists. Please select another date.",
+      });
+    }
+
     const MatcheFix = {
       opponent: `${team.name} VS ${body.team}`,
-      date: new Date(body.date),
+      date: matchDate,
       time: body.time,
       location: body.location,
     };
@@ -26,7 +39,6 @@ const get_Upcoming_Matches = async (req, res, next) => {
     if (!updatedTeam) {
       return res.status(404).json({ error: "Team not found" });
     }
-
     res.json({
       message: "Upcoming match added successfully",
       team: updatedTeam,
